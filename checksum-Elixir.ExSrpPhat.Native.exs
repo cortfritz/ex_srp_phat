8c6962f663c86f2ce05860c8a47a068ccaa10de6
Cort Fritz <cort@rheli.co>
2026-06-20 14:29:04 -0700

    chore: precompiled NIF checksums from cortfritz v0.1.0 release

--- checksum-Elixir.ExSrpPhat.Native.exs
+++ checksum-Elixir.ExSrpPhat.Native.exs
@@ -1,9 +1,9 @@
 %{
-  "libsrp_phat-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:7d0299e0b9f75a4d3cd54b69e435641fce3313dde9601786f19e0defdc02d350",
-  "libsrp_phat-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:5b158782284834c5b97a4812343b9d308f71529621633d85c918c7754dd865f0",
-  "libsrp_phat-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:c6c59e417c608a4ab2ab61eeaf673b853a574d2bdde7622e160c33fff6da52fd",
-  "libsrp_phat-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:023e882eeea50784625fa73f1227ffdfaa6dcdd07600ffb19faeb420ea1b9549",
-  "libsrp_phat-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:667fe00f1c7238f0e31b1cd5513005b87ff9cdefec0a4f4e367eb92b4beef6e8",
-  "libsrp_phat-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:092798077ba6e63f8e2110df95d0dfcf475da711d54763e66adbefb3036a4217",
-  "srp_phat-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:b48044fc989dacff93144a3ff0d2bcda465b6487f11ab47e7c0d3a2616d6fe9c",
+  "libsrp_phat-v0.1.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:742a2700c0e7cfe93fc1e6ef1e6eab87ab7cde6c893c420ee60942b6e76161b2",
+  "libsrp_phat-v0.1.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:56262cf6dd4b750c29bfc16d03e0b76ef04d0be215dc2efe045964bfd051f48d",
+  "libsrp_phat-v0.1.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:e427874cb625f19eef828146903c57c627261b34078773e5880fdeece0d13f78",
+  "libsrp_phat-v0.1.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:49d9ecae5502f50e988c3519f8a30b56d01d6fec3b973d6901461222b4889724",
+  "libsrp_phat-v0.1.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:f189ca4fe51f0046f63835d2dd1d1970940bd33a38bd0356733e083ce4a5fbd3",
+  "libsrp_phat-v0.1.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:03cdb707f0de9bf26ae23cc4104639dde0f09517e1b3e625e595ad01549552de",
+  "srp_phat-v0.1.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:03f42a4ce268348c2b2d9a9fa0c45c748d3a1b23d343b775ba418756015470f8",
 }
